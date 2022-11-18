329e0c1bb82114e81ac8ca6c7fb21bb61c957a23
roberio <roberio-almeida@hotmail.com>
2022-11-17 22:59:19 -0300

    build: redux tk for account api

--- app/frontend/src/app/store/account/apiService.ts
+++ app/frontend/src/app/store/account/apiService.ts
@@ -5,34 +5,20 @@ export const accountApi = createApi({
   reducerPath: "accountApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
   endpoints: (builder) => ({
-    getUserById: builder.query<IAccount, number>({
-      query: (id) => `/user/${id}`,
+    getAccountById: builder.query<IAccount, number>({
+      query: (id) => `/account/${id}`,
     }),
-    createUser: builder.mutation<IAccount, IAccount>({
-      query: (user) => ({
-        url: "/sign_up",
-        body: user,
+    createAccount: builder.mutation<IAccount, IAccount>({
+      query: () => ({
+        url: "/account",
         method: "POST",
       }),
     }),
-    loginUser: builder.mutation<IAccount, IAccount>({
-      query: (user) => ({
-        url: "/sign_in",
-        body: user,
-        method: "POST",
-      }),
-    }),
-    updateUser: builder.mutation<IAccount, IAccount>({
-      query: (user) => ({
-        url: `/user/${user.id}`,
-        body: user,
+    updateAccount: builder.mutation<IAccount, IAccount>({
+      query: (data) => ({
+        url: `/user/${data.id}`,
+        body: data,
         method: "PATCH",
-      }),
-    }),
-    deleteUser: builder.mutation<void, number>({
-      query: (id) => ({
-        url: `/users/${id}`,
-        method: "DELETE",
       }),
     }),
   }),
